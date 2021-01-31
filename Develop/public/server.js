975d42809c3ee762e6d1e2ca41ee8e3403c16916
Jessica Ferguson <jferguson1903@gmail.com>
2021-01-31 15:02:32 -0600

    routes for the landing & notes pages added

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -11,3 +11,10 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+// Basic route that sends the user to the landing page
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+
+// Basic route that sends the user first to the notes page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
