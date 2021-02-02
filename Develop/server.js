c0c43b063a69dbf8bf2890331a8204177bb15dae
Jessica Ferguson <jferguson1903@gmail.com>
2021-02-01 19:25:09 -0600

    repathed to include public since file was relocated

--- Develop/server.js
+++ Develop/server.js
@@ -13,19 +13,19 @@ app.use(express.json());
 
 // Routes
 //  Route to local assets folder
-app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/assets', express.static(__dirname + '/public/assets'))
 
 // 204 content code for Favicon as to not cause a 404 error in cosole log
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 // Basic route that sends the user to the landing page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 // Basic route that sends the user first to the notes page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
 // Sets default route to be landing page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 // TEST OBJECTS for loading purposes
 
